d4788a81ef4db6eddee41e6bfdf0646435c94add
Dan Codae <drcoda@gmail.com>
2018-08-09 15:17:52 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Add validation!');
+// console.log('Add validation!');
+document.getElementById('parking-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    var name = document.getElementById('name').value.trim()
+
+    if (name === "") {
+        alert('error')
+    }
+
+})
+
+function showInvalidFieldError () {
+    var errorDiv = document.createElement('div')
+}
