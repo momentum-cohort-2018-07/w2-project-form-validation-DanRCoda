37229c7374f3bb7303b081eb2bdbc882d4712b83
Dan Codae <drcoda@gmail.com>
2018-08-10 15:52:43 -0400

    finished step 2 clearing error msg

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // console.log('Add validation!');
 document.getElementById('parking-form').addEventListener('submit', function (event) {
     event.preventDefault()
+    clearError()
     var inputElements = document.getElementsByTagName('input')
     for (var el of inputElements) {
         
         if (el.value.trim() === "") {
             
            el.parentElement.classList.add('input-invalid')
+           el.parentElement.classList.remove('input-valid')
            errorMessage(el.parentElement)
+           
+           
            
            
           
            
         } else {
             el.parentElement.classList.add('input-valid')
+            el.parentElement.classList.remove('input-invalid')
         }
     }
 })
@@ -27,11 +32,9 @@ function errorMessage(el) {
 
 }
 
-function clearError(el) {
-    var input = document.getElementsByTagName('div')
-    div.classList.remove('errorDiv')
-    var errorMsg =div.parentElement.querySelector('error-msg')
-    if (errorMsg) {
-        errorMsg.remove(el)
+function clearError() {
+    var errorMsgs = document.querySelectorAll('.error-msg')
+    for (var errorMsg of errorMsgs) {
+        errorMsg.remove()
     }
 }
