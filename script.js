829b273803a36a3a0d1740be0b6bc8f7222b9bbe
Dan Codae <drcoda@gmail.com>
2018-08-09 21:33:01 -0400

    added input-valid function

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 // console.log('Add validation!');
-document.getElementById('parking-form').addEventListener('submit', function(event) {
+document.getElementById('parking-form').addEventListener('submit', function (event) {
     event.preventDefault()
-
-    var name = document.getElementById('name').value.trim()
-
-    if (name === "") {
-        alert('error')
+    var inputElements=document.querySelectorAll('input')
+    for(input of inputElements){console.log(input)
+    if (input.value.trim() === (""))
+        showError(event)
+    else  
+        showValid(event)
     }
-
+    
 })
 
-function showInvalidFieldError () {
+function showError() {
     var errorDiv = document.createElement('div')
+    errorDiv.classList.add('input-invalid')
+
+    var field = document.querySelector('.input-field')
+    // field.parentElement.appendChild(errorDiv)
+    field.classList.add('input-invalid')
+
+}
+
+function showValid() {
+    var validDiv = document.createElement('div')
+    validDiv.classList.add('input-valid')
+
+    var field = document.querySelector('.input-field')
+    field.classList.add('input-valid')
 }
